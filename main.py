4651213c7e2744f17010a48c8cab908688dfdced
hassansim <simhassan2015@gmail.com>
2023-05-13 21:37:36 +0530

    Added read_random_word function

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import random
+
 def print_menu():
     print("Let's play Wordle:")
     print("Type a 5 letter word and hit enter!")
-    
+
+def read_random_word():
+    with open("words.txt") as f:
+        words = f.read().splitlines()
+        return random.choice(words)
+
 print_menu()
