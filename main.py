801cf5346ee52f9943c16d47a5319b12f68bcbd2
hassansim <simhassan2015@gmail.com>
2023-05-13 23:18:13 +0530

    Added nltk library to load words

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import random
 import sys
 from termcolor import colored
+import nltk
+nltk.download('words')
+from nltk.corpus import words
 
 def print_menu():
     print("Let's play Wordle:")
@@ -11,23 +14,35 @@ def read_random_word():
         words = f.read().splitlines()
         return random.choice(words)
 
+nltk.data.path.append('/work/words')
+word_list = words.words()
+word_five = [word for word in word_list if len(word) == 5]
+
 print_menu()
-word = read_random_word()
+play_again = ""
+while play_again != "q":
+    # word = read_random_word()
+    word = random.choice(word_five)
+    for attempt in range(1, 7):
+        guess = input().lower()
 
-for attempt in range(1, 7):
-    guess = input().lower()
+        sys.stdout.write('\x1b[1A')
+        sys.stdout.write('\x1b[2K')
 
-    for i in range( min(len(guess), 5) ):
-        if guess[i] == word[i]:
-            print(colored(guess[i], 'green'), end="")
-        elif guess[i] in word:
-            print(colored(guess[i], 'yellow'), end="")
-        else:
-            print(guess[i], end="")
+        for i in range( min(len(guess), 5) ):
+            if guess[i] == word[i]:
+                print(colored(guess[i], 'green'), end="")
+            elif guess[i] in word:
+                print(colored(guess[i], 'yellow'), end="")
+            else:
+                print(guess[i], end="")
 
-        
-    if guess == word:
-        print(colored(f"Congrats! You got the wordle in {attempt}", 'red'))
-        break
+        print()   
 
+        if guess == word:
+            print(colored(f"Congrats! You got the wordle in {attempt}", 'red'))
+            break
+        elif attempt == 6:
+            print(f"Sorry the wordle was... {word}")
+    play_again = input("Want to play again? Type q to exit : ")
 
